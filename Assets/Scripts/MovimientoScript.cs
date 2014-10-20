b5127a4a0e1573998d75bee792545fb7b212fb37
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-10-20 17:52:34 +0200

    columnas

---
+++ Assets/Scripts/MovimientoScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoScript : MonoBehaviour {
+
+	public int vhorizontal = 10;
+	public int vvertical = 10;
+	Vector3 movimiento;
+	Vector3 posicionRaton;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Lineas par mover con teclado o joystick
+		//var v = Input.GetAxis ("Vertical");
+		//var h = Input.GetAxis ("Horizontal");
+		//movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
+		//transform.Translate (movimiento*Time.deltaTime);
+
+		//Player sigue el movimiento del raton
+		posicionRaton = Input.mousePosition;
+		posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
+
+		//transform.position = Vector2.Lerp (transform.position,
+		                                   //posicionRaton,
+		                                   //vvertical);
+		if (Input.GetMouseButton (0)) {
+				transform.position = Vector2.Lerp (transform.position,
+			                                   posicionRaton,
+			                                   vvertical*0.01f);
+		}		
+
+	
+	}
+}
