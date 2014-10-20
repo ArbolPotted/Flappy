245b9d0c910830cd4e87efdf92a6480fe57b543d
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-10-20 18:44:12 +0200

    culmnas

---
+++ Assets/Scripts/generadorColumnas.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generadorColumnas : MonoBehaviour {
+
+	public GameObject columna;
+	public float tiempoespera = 3;
+	float timer = 0;
+	Vector3 posicion;
+
+	// Use this for initialization
+	void Start () {
+		posicion = transform.position;
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > timer) {
+			var altura = Random.Range(-2f, 2f);
+
+			var nuevacolumna = (GameObject) Instantiate(
+				columna,
+				new Vector3 (posicion.x,posicion.y+altura,posicion.z),
+				transform.rotation);
+			Destroy (nuevacolumna, tiempoespera*3);
+			timer = Time.time+tiempoespera;
+				}
+	
+	}
+}
