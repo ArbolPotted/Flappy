351e15e7f8ef975d2e3eec066adbc1020e4a1d17
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-10-22 16:18:41 +0200

    columna

--- Assets/Scripts/generadorColumnas.cs
+++ Assets/Scripts/generadorColumnas.cs
@@ -19,7 +19,7 @@ public class generadorColumnas : MonoBehaviour {
 	void Update () {
 
 		if (Time.time > timer) {
-			var altura = Random.Range(-1.5f, 6f);
+			var altura = Random.Range(-1f, 6f);
 
 			var nuevacolumna = (GameObject) Instantiate(
 				columna,
