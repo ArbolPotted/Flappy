4744424e5084d704c079f79652513c4e94665ed4
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-10-27 18:56:15 +0100

    nivel2

--- Assets/Scripts/MovimientoScript.cs
+++ Assets/Scripts/MovimientoScript.cs
@@ -5,6 +5,7 @@ public class MovimientoScript : MonoBehaviour {
 
 	public int vhorizontal = 10;
 	public int vvertical = 10;
+
 	Vector3 movimiento;
 	Vector3 posicionRaton;
 
@@ -16,24 +17,24 @@ public class MovimientoScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Lineas par mover con teclado o joystick
-		//var v = Input.GetAxis ("Vertical");
-		//var h = Input.GetAxis ("Horizontal");
-		//movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
-		//transform.Translate (movimiento*Time.deltaTime);
+		var v = Input.GetAxis ("Vertical");
+		var h = Input.GetAxis ("Horizontal");
+		movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
+		transform.Translate (movimiento *Time.deltaTime);
 
 		//Player sigue el movimiento del raton
-		posicionRaton = Input.mousePosition;
-		posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
+		//posicionRaton = Input.mousePosition;
+		//posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
 
 		//transform.position = Vector2.Lerp (transform.position,
 		                                   //posicionRaton,
-		                                   //vvertical);
-		if (Input.GetMouseButton (0)) {
-				transform.position = Vector2.Lerp (transform.position,
-			                                   posicionRaton,
-			                                   vvertical*0.01f);
+		                                   //vvertical); 
+		//if (Input.GetMouseButton (0)) {
+				//transform.position = Vector2.Lerp (transform.position,
+			                                   //posicionRaton,
+			                                   //vvertical*0.01f);
 		}		
 
 	
 	}
-}
+
