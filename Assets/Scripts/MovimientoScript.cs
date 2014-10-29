7b46d81e58aa099f8b1e46bdf0455f667aa1b2fa
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-10-29 18:53:37 +0100

    todo me mata D8

--- Assets/Scripts/MovimientoScript.cs
+++ Assets/Scripts/MovimientoScript.cs
@@ -9,32 +9,55 @@ public class MovimientoScript : MonoBehaviour {
 	Vector3 movimiento;
 	Vector3 posicionRaton;
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Lineas par mover con teclado o joystick
-		var v = Input.GetAxis ("Vertical");
-		var h = Input.GetAxis ("Horizontal");
-		movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
-		transform.Translate (movimiento *Time.deltaTime);
+		if (GameControl.dead == false) {
 
-		//Player sigue el movimiento del raton
-		//posicionRaton = Input.mousePosition;
-		//posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
+						//Lineas par mover con teclado o joystick
+						var v = Input.GetAxis ("Vertical");
+						var h = Input.GetAxis ("Horizontal");
+						movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
+						transform.Translate (movimiento * Time.deltaTime);
+				}
 
-		//transform.position = Vector2.Lerp (transform.position,
-		                                   //posicionRaton,
-		                                   //vvertical); 
-		//if (Input.GetMouseButton (0)) {
+				//Player sigue el movimiento del raton
+				//posicionRaton = Input.mousePosition;
+				//posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
+
 				//transform.position = Vector2.Lerp (transform.position,
-			                                   //posicionRaton,
-			                                   //vvertical*0.01f);
-		}		
+				//posicionRaton,
+				//vvertical); 
+				//if (Input.GetMouseButton (0)) {
+				//transform.position = Vector2.Lerp (transform.position,
+				//posicionRaton,
+				//vvertical*0.01f);
+		}
 
-	
+		void OnCollisionEnter2D(Collision2D coll){
+
+			if (coll.gameObject.tag == "Enemy")
+				GameControl.score = GameControl.score -1;
+			
+			if (coll.gameObject.tag == "Points")
+				GameControl.score = GameControl.score +1;
+
+			if (coll.gameObject.tag == "Win")
+				Application.LoadLevel(Application.loadedLevel);
+
+			if (coll.gameObject.tag == "Enemy" && GameControl.score == 0)
+				GameControl.dead = true;
+				anim.SetBool("Muerte",true);
+				Application.LoadLevel(Application.loadedLevel);
+			
+			}		
+
 	}
+
 
